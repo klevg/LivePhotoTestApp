860ec754cfe57b1db267cd81dd5c7f6026576e6c
Евгений Клебан <kleban_eugene@tut.by>
2019-03-27 17:56:41 +0300

    initial commit

---
+++ LivePhotoTestApp/LivePhotoTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LivePhotoTestApp
+//
+//  Created by Евгений Клебан on 3/27/19.
+//  Copyright © 2019 Евгений Клебан. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
